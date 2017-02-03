3955497a1680d8a38a09558310a04f5a61673faa
Storskorpan <michael.gustavsson@skaftoskorpan.se>
2017-02-03 22:13:32 +0100

    Initial commit

---
+++ app/course/course.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+import { Course } from './course';
+import { COURSES } from './../data/mock-data';
+
+@Injectable()
+export class CourseService {
+
+  getCourses(): Promise<Course[]> {
+    return Promise.resolve(COURSES);
+  }
+}
